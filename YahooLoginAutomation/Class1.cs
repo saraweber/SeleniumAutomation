6108c32a83787840a9c03a3c3142e0d53277a452
Sara Fox <safox2015@gmail.com>
2016-02-18 16:04:53 -0600

    commit

---
+++ YahooLoginAutomation/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Chrome;
+
+namespace YahooLoginAutomation
+{
+    public class Class1
+    {
+        public void Go()
+        {
+            var driver = new ChromeDriver ();
+            driver.Navigate().GoToUrl("https://login.yahoo.com/config/login?.src=fpctx&.intl=us&.lang=en-US&.done=https%3A%2F%2Fwww.yahoo.com");
+        }
+    }
+}
---
+++ YahooTests/UnitTest1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using YahooLoginAutomation;
+
+namespace YahooTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+         var c = new Class1();
+         c.Go();
+        }
+    }
+}
